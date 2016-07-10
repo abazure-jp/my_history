2e050d5200267c4c5104df3d2f6e9adfb408a1b8
say <say.0213@gmail.com>
2016-07-10 17:15:06 +0900

    既出コマンドなら古い方を.bash_historyから消してくれるコマンド(´ᕦ∀ᕤ｀)

--- checkEndStatus.sh
+++ checkEndStatus.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-# $?は直前のコマンドの終了ステータスを保持している。
-# 0なら成功
-# 1なら失敗
-# 127だとそんなコマンドねーよになる。
-if test $? -eq 0 ; then
-  echo "(U^ω^)"
-else
-  history -d $(( $HISTCMD - 1 ))
-  echo "(´；ω；｀)"
-fi
+echo  $(( $HISTCMD -1 ))
+echo  $(( $HISTCMD -1 ))
+echo  $(( $HISTCMD -1 ))
